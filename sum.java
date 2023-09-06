08ad25f9c0716d5a645314948bc3eb0ce06ca8e6
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-09-06 23:55:51 +0530

    Sum two numbers

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class sum {
+    public static void main(String[] args){
+
+
+                int x = 10;
+                int y = 20;
+                int z = 70;
+                int total = x+y+z;
+                System.out.print(total);
+
+
+    }
+}
