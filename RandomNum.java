7c45e468595c8021b1960eb8677c39155e3606c0
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-09-10 23:55:37 +0530

    Random number genrator

---
+++ RandomNum.java
@@ -0,0 +1,10 @@
+//import java.util.Scanner;
+import java.util.Random;
+
+public class RandomNum {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int a = r.nextInt();
+        System.out.println(a);
+    }
+}
