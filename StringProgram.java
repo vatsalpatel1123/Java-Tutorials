40152e3bd747125ce05f2027fa1085d485d889db
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-09-11 23:29:40 +0530

    String Program

---
+++ StringProgram.java
@@ -0,0 +1,31 @@
+public class StringProgram {
+
+        public static void main(String[] args) {
+
+            String name = "Jack Parker";
+            name = name.toLowerCase();
+            System.out.println(name);
+
+
+            String text = "To My     Friend";
+            text = text.replace(" ", "_");
+            System.out.println(text);
+
+         
+            String letter = "Dear <|name|>, Thanks a lot!";
+            letter = letter.replace("<|name|>", "Sachin");
+            System.out.println(letter);
+
+
+            String myString = "This string contains double and  triple spaces";
+            System.out.println(myString.indexOf("  "));
+            System.out.println(myString.indexOf("   "));
+
+
+            String myLetter = "Dear Harry,\n\tThis Java Course is Nice.\nThanks!";
+            System.out.println(myLetter);
+
+        }
+    }
+
+}
