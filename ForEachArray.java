1f287e476a6b89995332377e4b8159323c593d2f
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-09-12 23:34:07 +0530

    Find Array Element using For each loop

---
+++ ForEachArray.java
@@ -0,0 +1,29 @@
+public class ForEachArray {
+
+
+        public static void main(String[] args) {
+
+        float [] marks = {45.7f, 67.8f, 63.4f, 99.2f, 100.0f};
+        float num = 45.57f;
+        boolean isInArray = false;
+        for(float element:marks){
+            if(num==element){
+                isInArray = true;
+                break;
+            }
+        }
+        if(isInArray){
+            System.out.println("The value is present in the array");
+        }
+        else{
+            System.out.println("The value is not present in the array");
+        }
+
+
+
+
+
+        }
+    }
+
+
