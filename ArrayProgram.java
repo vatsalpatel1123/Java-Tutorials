48b1fac7a2a76b0b63e37395e9ca43bf518c0742
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-09-12 23:29:48 +0530

    Array Program

---
+++ ArrayProgram.java
@@ -0,0 +1,15 @@
+public class ArrayProgram {
+
+        public static void main(String[] args) {
+
+         float [] marks = {45.7f, 67.8f, 63.4f, 99.2f, 100.0f};
+        float sum = 0;
+        for(float element:marks){
+            sum = sum + element;
+        }
+        System.out.println("The value of sum is " + sum);
+
+        }
+    }
+
+
