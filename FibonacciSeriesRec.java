8adb8e3a18ee5f00cbbb7e3ac78c88e6ec2ca347
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-09-13 23:49:59 +0530

    nth term of the Fibonacci series using recursion

---
+++ FibonacciSeriesRec.java
@@ -0,0 +1,22 @@
+public class FibonacciSeriesRec {
+
+
+
+        static int fib(int n) {
+
+            if (n == 1 || n == 2) {
+                return n - 1;
+            } else {
+                return fib(n - 1) + fib(n - 2);
+            }
+        }
+
+        public static void main(String[] args) {
+
+             int result = fib(7);
+             System.out.println(result);
+
+        }
+    }
+
+
