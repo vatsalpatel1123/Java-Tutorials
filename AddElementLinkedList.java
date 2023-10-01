b787097b85d70592249917291927369ccaec1862
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-10-01 19:22:36 +0530

    Adding Element in LinkedList

---
+++ AddElementLinkedList.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AddElementLinkedList extends Thread{
+    public static void main(String[] args) {
+
+        LinkedList<Integer> l1 = new LinkedList<>();
+
+        l1.add(11);
+        l1.add(22);
+        l1.add(33);
+        l1.add(44);
+        l1.add(55);
+        l1.add(77);
+        l1.add(5,77); // Inserts 77 at index 5
+        System.out.println("L1 Linked list : "+ l1);
+
+    }
+}
