df6ac30064ab9eded9b62feddffef3716a2323dd
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-10-02 23:21:14 +0530

    Inserting an Element in ArrayDeque

---
+++ InsertingElementArrayDeque.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class InsertingElementArrayDeque extends Thread{
+    public static void main(String[] args) {
+
+        ArrayDeque<Integer> ad1 = new ArrayDeque<>();
+        ad1.add(6);
+        ad1.add(56);
+        ad1.add(9);
+        ad1.addFirst(5);
+        ad1.offerFirst(10);
+        System.out.println(ad1);
+
+    }
+}
+
