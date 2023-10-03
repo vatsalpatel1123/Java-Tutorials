23390f85d417e3ea304ed797464eef680baf5b51
Vatsal Patel <iamvatsallpatell@gmail.com>
2023-10-03 23:16:18 +0530

    The Date Class in Java

---
+++ DateClass.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class DateClass extends Thread{
+    public static void main(String[] args) {
+        System.out.println("The maximum value of long is :" + Long.MAX_VALUE);
+        System.out.println("The value of current time in ms : " + System.currentTimeMillis());
+    }
+}
